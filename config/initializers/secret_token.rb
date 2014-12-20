a1a7a01a918e1d55d87a52ae5673ab13b55e84c6
18sasaki <18sasaki@gmail.com>
2014-12-20 14:39:24 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MassageApi::Application.config.secret_key_base = '4a589bf0f8fcb04fd543e5c729cac2ebcf2a3a18b6e4b03d9bd0e2830c8f1d7e50f7133b635624d839256788e52f88b5c263b65a4ce529fbb3b5aeceea2e788a'
