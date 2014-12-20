38bb9505fa806241c4cea0cf20b1f2604d15f30a
18sasaki <18sasaki@gmail.com>
2014-12-20 18:31:58 +0900

    ログイン成功時にuser_typeも返す

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -22,12 +22,13 @@ class Api::UsersController < Api::ApiBaseController
     user = User.find_by(user_id: params[:user_id])
     if user.password == Digest::SHA1.hexdigest("#{user.salt}--#{params[:password]}--}")
       result = 'true'
+      user_type = user.user_type
     else
       result = 'false'
     end
   rescue
     result = 'false'
   ensure
-    render json: {result: result}
+    render json: {result: result, user_type: user_type}
   end
 end
